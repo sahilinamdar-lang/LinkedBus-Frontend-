7f29fe1672ba80816b411f84bcd708a189fab109
Sahil inamdar <sahilinamdar212@gmail.com>
2025-11-23 17:02:33 +0530

    Update API_BASE_URL to remove trailing slash
    Removed trailing slash from API base URL.

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://linkedbus-backend-production.up.railway.app/'; // Assuming your Spring Boot runs on port 8080
+export const API_BASE_URL = 'https://linkedbus-backend-production.up.railway.app';
